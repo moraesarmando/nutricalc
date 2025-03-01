78c8e24a88c43582b0781bf66cf0e373bf9edee0
armando.moraes <armando.moraes@ufca.edu.br>
2025-03-01 15:29:21 -0300

    refatoração com base em fórmulas

--- scripts.js
+++ scripts.js
@@ -1,39 +1,48 @@
-function calcularAltura() {
-    const genero = document.getElementById('genero').value;
-    const idade = parseInt(document.getElementById('idade').value);
-    const tibia = parseFloat(document.getElementById('tibia').value);
-    let altura;
-    
-    if (isNaN(idade) || idade <= 0 || isNaN(tibia) || tibia <= 0) {
-        document.getElementById('resultado-altura').innerText = "Por favor, insira valores válidos.";
-        return;
-    }
-    
-    if (genero === "masculino") {
-        altura = ((2.03 * tibia) - (0.04 * idade)) + 64.19;
-    } else {
-        altura = ((1.83 * tibia) - (0.24 * idade) + 84.88);
-    }
-    
-    document.getElementById('resultado-altura').innerText = `Altura estimada: ${altura.toFixed(2)} cm`;
-}
+function calcularPesoAJCB() {
+  const idade = parseInt(document.getElementById('idade').value);
+  const genero = document.getElementById('genero1').value;
+  const etnia = document.getElementById('etnia').value;
+  const aj = parseFloat(document.getElementById('aj').value);
+  const cb = parseFloat(document.getElementById('cb').value);
+  let peso;
 
-function calcularPeso() {
-    const genero = document.getElementById('genero').value;
-    const braco = parseFloat(document.getElementById('braco').value);
-    const panturrilha = parseFloat(document.getElementById('panturrilha').value);
-    let peso;
-    
-    if (isNaN(braco) || braco <= 0 || isNaN(panturrilha) || panturrilha <= 0) {
-        document.getElementById('resultado-peso').innerText = "Por favor, insira valores válidos.";
-        return;
-    }
-    
-    if (genero === "masculino") {
-        peso = (0.98 * braco) + (1.16 * panturrilha) + 1.73;
-    } else {
-        peso = (1.09 * braco) + (1.50 * panturrilha) - 3.32;
-    }
-    
-    document.getElementById('resultado-peso').innerText = `Peso estimado: ${peso.toFixed(2)} kg`;
+  if (isNaN(idade) || idade <= 0 || isNaN(aj) || aj <= 0 || isNaN(cb) || cb <= 0) {
+      document.getElementById('resultado-peso-ajcb').innerText = "Por favor, insira valores válidos.";
+      return;
+  }
+
+  if (idade >= 18 && idade <= 60) {
+      if (genero === "masculino") {
+          if (etnia === "branco") {
+              peso = (aj * 1.19) + (cb * 3.21) - 86.82;
+          } else {
+              peso = (aj * 1.09) + (cb * 3.14) - 83.72;
+          }
+      } else {
+          if (etnia === "branco") {
+              peso = (aj * 1.01) + (cb * 2.81) - 60.04;
+          } else {
+              peso = (aj * 1.24) + (cb * 2.97) - 82.48;
+          }
+      }
+  } else if (idade > 60) {
+      if (genero === "masculino") {
+          if (etnia === "branco") {
+              peso = (aj * 1.10) + (cb * 3.07) - 75.81;
+          } else {
+              peso = (aj * 0.44) + (cb * 2.86) - 39.21;
+          }
+      } else {
+          if (etnia === "branco") {
+              peso = (aj * 1.09) + (cb * 2.68) - 65.51;
+          } else {
+              peso = (aj * 1.50) + (cb * 2.58) - 84.22;
+          }
+      }
+  } else {
+      document.getElementById('resultado-peso-ajcb').innerText = "Idade fora da faixa de cálculo.";
+      return;
+  }
+
+  document.getElementById('resultado-peso-ajcb').innerText = `Peso estimado: ${peso.toFixed(2)} kg`;
 }
